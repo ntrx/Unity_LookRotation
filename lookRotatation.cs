d6d5e79fb074892d8d7d0b7b7cb0264f7172921b
Bohdan <ntrxhm@gmail.com>
2023-04-13 14:40:26 +0300

    isHit

--- lookRotatation.cs
+++ lookRotatation.cs
@@ -4,19 +4,30 @@ public class lookRot : MonoBehaviour
 {
     RaycastHit hit;
     Transform _followThis;
+    public bool isTracking = false;
+    public bool isHit = false;
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000f)) {
+        if (isTracking) {
+            Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 1000f);
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
 
-            this._followThis = hit.collider.transform;
+            if (hit.collider != null)
+                _followThis = hit.collider.transform;
+            if (hit.collider.tag == "target")
+                isHit = true;
+            else
+                isHit = false;
 
-            Vector3 relPos = this._followThis.position - transform.position;
-            Quaternion lRot = Quaternion.LookRotation(relPos);
-            transform.rotation = lRot;
+            if (isHit) {
+                Vector3 relPos = _followThis.position - transform.position;
+                Quaternion lRot = Quaternion.LookRotation(relPos);
+                transform.rotation = lRot;
+            }
+            
         }
     }
 }
